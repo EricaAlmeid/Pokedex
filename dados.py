aa9ccb8e2437946fe6a312d8c3ad995a7b8d0f9c
EricaAlmeid <115961939+EricaAlmeid@users.noreply.github.com>
2022-10-19 23:13:41 -0300

    05

---
+++ dados.py
@@ -0,0 +1,54 @@
+
+
+pokemon={
+    'JigglyPuff': {
+        "status": ['HP: 115', 'Ataque: 45', 'Defesa: 20', 'Velocidade: 20','Total: 180'],
+        "habilidades": ['Cute Charm','Competitive'],
+        "tipo": ['#039', 'Balão', 'images/jigglypuff.png'],
+
+    },
+
+    'Pikatchu': {
+        "status": ['HP: 300', 'Ataque: 600', 'Defesa: 500', 'Velocidade: 300', 'Total: 1.700'],
+        "habilidades": ['Choque do trovão', 'Cabeçada'],
+        "tipo": ['#025', 'elétrico', 'images/pikachu.png'],
+
+    },
+
+    'Bulbasaur': {
+        "status": ['HP: 320', 'Ataque: 510', 'Defesa: 400', 'Velocidade: 200', 'Total: 1.430'],
+        "habilidades": ['Folhas navalha', ' solar'],
+        "tipo": ['#001', 'grama', 'images/bulbasaur.png'],
+
+    },
+
+    'Charmander': {
+        "status": ['HP: 200', 'Ataque: 300', 'Defesa: 400', 'Velocidade: 320', 'Total: 1.220'],
+        "habilidades": ['Lança-chamas', 'Ataque rápido'],
+        "tipo": ['#004', 'fogo', 'images/charmander.png'],
+
+    },
+
+    'Gyarados': {
+        "status": ['HP: 300', 'Ataque: 600', 'Defesa: 500', 'Velocidade: 300', 'Total: 1.700'],
+        "habilidades": ['Jato dÁgua', 'Hidro bomba'],
+        "tipo": ['#130', 'água', 'images/gyarados.png'],
+
+    },
+
+    'Gengar': {
+        "status": ['HP: 100', 'Ataque: 200', 'Defesa: 300', 'Velocidade: 400', 'Total: 1.000'],
+        "habilidades": ['Bola sombria', 'Lambida'],
+        "tipo": ['#094', 'trevas', 'images/gengar.png'],
+
+    },
+
+    'Dragonnite': {
+        "status": ['HP: 500', 'Ataque: 600', 'Defesa: 400', 'Velocidade: 400', 'Total: 1.900'],
+        "habilidades": ['Dança do dragão', 'Velocidade extrema'],
+        "tipo": ['#149', 'dragão', 'images/dragonite.png'],
+
+    }
+
+
+}
--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from tkinter import *
 from tkinter import ttk
 
 from PIL import Image, ImageTk
+from dados import*
+
+
+
 
 co0 = "#444466"  # preto
 co1 = "#feffff"  # branca
@@ -25,6 +29,16 @@ style.theme_use("clam")
 #### Criando quadro ###
 quadro_pokemon = Frame(janela, width=500, height=290, relief='flat')
 quadro_pokemon.grid(row=1, column=0)
+
+def trocar_pokemon(i):
+    global imagem_pokemon, pok_imegem
+
+    ##tipo de pokemon##
+    pok_nome['text'] = i
+    pok_tipo['text'] = pokemon[i]['tipo'][1]
+    pok_identificacao['text']=pokemon[i]['tipo'][0]
+
+
 
 ### nome ####
 pok_nome = Label(quadro_pokemon, text='Jigglypuff', relief='flat', anchor=CENTER, font=('Fixedsys 20'), bg=co1, fg=co0)
@@ -96,55 +110,64 @@ imagem_pok_1 = Image.open('images/cabeca_jigglypuff.png')
 imagem_pok_1 = imagem_pok_1.resize((40, 40))
 imagem_pok_1 = ImageTk.PhotoImage(imagem_pok_1)
 
-b_pok_1 = Button(janela, image=imagem_pok_1, text='JigglyPuff', width=150, relief='raised', overrelief=RIDGE, compound=LEFT, anchor=NW,  padx=5, font=('Arial 12'), bg=co1, fg=co0)
+b_pok_1 = Button(janela, command=lambda: trocar_pokemon('JigglyPuff'), image=imagem_pok_1, text='JigglyPuff', width=150, relief='raised', overrelief=RIDGE, compound=LEFT, anchor=NW,  padx=5, font=('Arial 12'), bg=co1, fg=co0)
 b_pok_1.place(x=375, y=10)
 
 ### Botao 2 ###
 
-imagem_pok_2 = Image.open('images/cabeca_jigglypuff.png')
+imagem_pok_2 = Image.open('images/cabeca-pikachu.png')
 imagem_pok_2 = imagem_pok_2.resize((40, 40))
 imagem_pok_2 = ImageTk.PhotoImage(imagem_pok_2)
 
-b_pok_2 = Button(janela, image=imagem_pok_2, text='JigglyPuff', width=150, relief='raised', overrelief=RIDGE, compound=LEFT, anchor=NW,  padx=5, font=('Arial 12'), bg=co1, fg=co0)
-b_pok_2.place(x=375, y=10)
+b_pok_2 = Button(janela,  command=lambda: trocar_pokemon('Pikachu'), image=imagem_pok_2, text='Pikachu', width=150, relief='raised', overrelief=RIDGE, compound=LEFT, anchor=NW,  padx=5, font=('Arial 12'), bg=co1, fg=co0)
+b_pok_2.place(x=375, y=65)
 
 
 ### Botao 3 ###
 
-imagem_pok_3 = Image.open('images/cabeca_jigglypuff.png')
+imagem_pok_3 = Image.open('images/cabeca-bulbasaur.png')
 imagem_pok_3 = imagem_pok_3.resize((40, 40))
 imagem_pok_3 = ImageTk.PhotoImage(imagem_pok_3)
 
-b_pok_3 = Button(janela, image=imagem_pok_3, text='JigglyPuff', width=150, relief='raised', overrelief=RIDGE, compound=LEFT, anchor=NW,  padx=5, font=('Arial 12'), bg=co1, fg=co0)
-b_pok_3.place(x=375, y=10)
+b_pok_3 = Button(janela, command=lambda: trocar_pokemon('Bulbasaur'), image=imagem_pok_3, text='Bulbasaur', width=150, relief='raised', overrelief=RIDGE, compound=LEFT, anchor=NW,  padx=5, font=('Arial 12'), bg=co1, fg=co0)
+b_pok_3.place(x=375, y=120)
 
 ### Botao 4 ###
 
-imagem_pok_4 = Image.open('images/cabeca_jigglypuff.png')
+imagem_pok_4 = Image.open('images/cabeca-charmander.png')
 imagem_pok_4 = imagem_pok_4.resize((40, 40))
 imagem_pok_4 = ImageTk.PhotoImage(imagem_pok_4)
 
-b_pok_4 = Button(janela, image=imagem_pok_4, text='JigglyPuff', width=150, relief='raised', overrelief=RIDGE, compound=LEFT, anchor=NW,  padx=5, font=('Arial 12'), bg=co1, fg=co0)
-b_pok_4.place(x=375, y=10)
+b_pok_4 = Button(janela, command=lambda: trocar_pokemon('Charmander'), image=imagem_pok_4, text='Charmander', width=150, relief='raised', overrelief=RIDGE, compound=LEFT, anchor=NW,  padx=5, font=('Arial 12'), bg=co1, fg=co0)
+b_pok_4.place(x=375, y=175)
 
 
 ### Botao 5 ###
 
-imagem_pok_5 = Image.open('images/cabeca_jigglypuff.png')
+imagem_pok_5 = Image.open('images/cabeca-gyarados.png')
 imagem_pok_5 = imagem_pok_5.resize((40, 40))
 imagem_pok_5 = ImageTk.PhotoImage(imagem_pok_5)
 
-b_pok_5 = Button(janela, image=imagem_pok_5, text='JigglyPuff', width=150, relief='raised', overrelief=RIDGE, compound=LEFT, anchor=NW,  padx=5, font=('Arial 12'), bg=co1, fg=co0)
-b_pok_5.place(x=375, y=10)
+b_pok_5 = Button(janela, command=lambda: trocar_pokemon('Gyarados'), image=imagem_pok_5, text='Gyarados', width=150, relief='raised', overrelief=RIDGE, compound=LEFT, anchor=NW,  padx=5, font=('Arial 12'), bg=co1, fg=co0)
+b_pok_5.place(x=375, y=230)
 
 ### Botao 6 ###
 
-imagem_pok_6 = Image.open('images/cabeca_jigglypuff.png')
+imagem_pok_6 = Image.open('images/cabeca-gengar.png')
 imagem_pok_6 = imagem_pok_6.resize((40, 40))
 imagem_pok_6 = ImageTk.PhotoImage(imagem_pok_6)
 
-b_pok_6 = Button(janela, image=imagem_pok_6, text='JigglyPuff', width=150, relief='raised', overrelief=RIDGE, compound=LEFT, anchor=NW,  padx=5, font=('Arial 12'), bg=co1, fg=co0)
-b_pok_6.place(x=375, y=10)
+b_pok_6 = Button(janela, command=lambda: trocar_pokemon('Gengar'), image=imagem_pok_6, text='Gengar', width=150, relief='raised', overrelief=RIDGE, compound=LEFT, anchor=NW,  padx=5, font=('Arial 12'), bg=co1, fg=co0)
+b_pok_6.place(x=375, y=285)
+
+### Botao 7 ###
+
+imagem_pok_7 = Image.open('images/cabeca-dragonite.png')
+imagem_pok_7 = imagem_pok_7.resize((40, 40))
+imagem_pok_7 = ImageTk.PhotoImage(imagem_pok_7)
+
+b_pok_6 = Button(janela, command=lambda: trocar_pokemon('Dragonite'), image=imagem_pok_7, text='Dragonnite', width=150, relief='raised', overrelief=RIDGE, compound=LEFT, anchor=NW,  padx=5, font=('Arial 12'), bg=co1, fg=co0)
+b_pok_6.place(x=375, y=340)
 
 
 janela.mainloop()
